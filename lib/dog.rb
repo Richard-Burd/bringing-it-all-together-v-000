a845a04819b1e41cb089c728fd715d519bc44508
Richard Burd <richardburdx@yahoo.com>
2017-10-12 14:51:25 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,43 @@
+
+
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+    self.id = id
+    self.name = name
+    self.breed = breed
+  end
+
+  def self.create_table
+    table_creating_sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(table_creating_sql)
+  end
+
+  def self.drop_table
+    table_destroying_sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+    SQL
+
+    DB[:conn].execute(table_destroying_sql)
+  end
+
+  def save
+    # This method requires the <<self.update>> in order to work.
+  end
+
+  def self.create # this only creates the Ruby object, not the database entry
+
+  end
+
+  def self.find_or_create_by
+    # This method requires the <<self.create>> method
+  end
+end
