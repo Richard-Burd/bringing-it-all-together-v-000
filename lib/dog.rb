d1b19061125101405b9222a7bada741739520bd7
Richard Burd <richardburdx@yahoo.com>
2017-10-12 16:21:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,7 +48,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-
+    id = row[0]
+    name = row[1]
+    breed = breed[2] 
+    self.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_name(name)
@@ -56,6 +59,12 @@ class Dog
   end
 
   def self.find_by_id(id)
+    sql_finder = <<-SQL 
+      SELECT * FROM dogs WHERE id = ? LIMIT 1 
+    SQL
 
+    DB[:conn].execute(sql_finder, id).map do |series| 
+      self.new_from_db(series) 
+    end.first
   end
 end
